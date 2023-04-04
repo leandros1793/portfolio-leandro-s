8c9c776010b7a3d888ba9f89d023fe169e991140
leandro-santiago <69202675+leandros1793@users.noreply.github.com>
2023-04-04 03:36:43 -0300

    Headers socials en header css

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+
+
 const Header = () => {
   return (
-    <div>
-      Header
-    </div>
+   <header>
+<div className=".container.header__container"></div>
+
+<h5>Hola Yo Soy </h5>
+<h1>Leandro Santiago</h1>
+<h5 className="text-light">Fullstack Developer</h5>
+
+<CTA/>
+
+
+   </header>
   )
 }
 
