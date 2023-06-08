9133b22b7393511f07869b4cc2f579f2f6f0696b
leandro-santiago <69202675+leandros1793@users.noreply.github.com>
2023-06-08 04:28:22 -0300

    finalizado casi

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
+import ME from '../../assets/me.png'
+import HeaderSocials from './HeaderSocials'
 
 
 const Header = () => {
@@ -13,6 +15,12 @@ const Header = () => {
 <h5 className="text-light">Fullstack Developer</h5>
 
 <CTA/>
+<HeaderSocials/>
+<div className='me'>
+  <img src={ME} alt="me" />
+
+</div>
+<a href="#contact" className='scroll__down'>Scroll Down</a>
 
 
    </header>
