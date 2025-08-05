0ceb77a9fb07ef2a12c637beb1470337e94e7a5e
leandros1793 <leandro.s.cba@gmail.com>
2025-08-05 15:51:21 -0300

    sin anuncios

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,21 +4,7 @@ import CTA from './CTA';
 import HeaderSocials from './HeaderSocials';
 
 const Header = () => {
-  useEffect(() => {
-    // Agregar el script de Popunder
-    const popunderScript = document.createElement('script');
-    popunderScript.src = '//pl25525519.profitablecpmrate.com/6a/ee/fd/6aeefd12ca6809a107db70d1c75422e7.js';
-    popunderScript.type = 'text/javascript';
-    popunderScript.async = true;
 
-    // Agregar el script al header
-    document.querySelector('header').appendChild(popunderScript);
-
-    // Limpiar el script al desmontar el componente
-    return () => {
-      document.querySelector('header').removeChild(popunderScript);
-    };
-  }, []);
 
   return (
     <header id="home">
